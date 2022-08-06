a6aa324c2aeb25839cecc2cdb9467a1285946571
Luis Torres <lgtorres42@gmail.com>
2022-08-05 22:41:12 -0500

    Added things and fixing bugs to ecs_test

--- src/ecs_test.cpp
+++ src/ecs_test.cpp
@@ -1,4 +1,7 @@
 #include <bitset>
+#include <cassert>
+#include <vector>
+#include <string>
 
 typedef int32_t EntityIndex;
 typedef int32_t EntityVersion;
@@ -25,7 +28,7 @@ public:
     }
     bool operator!=(EntityId rhs) const {
         return !(*this == rhs);
-    }
+    }    
 private:
     int64_t _id;
 };
@@ -38,7 +41,8 @@ int constexpr kMaxEntities = 256;
 class ComponentPool {
 public:
     void Init(int elementSize) {
-        assert(_pdata == nullptr);
+        assert(_pData == nullptr);
+        _elementSize = elementSize;
         _pData = new char[kMaxEntities * elementSize];
     }
     ~ComponentPool() {
@@ -59,9 +63,13 @@ public:
     struct EntityInfo {
         EntityId _id;
         ComponentMask _mask;
+    };
+
+    Scene() {
+        _componentPools.reserve(kMaxComponents);
     }
 
-    EntityId AddEntity() {
+    EntityId AddEntity() {        
         if (_freeEntities.empty()) {
             EntityId newId(_entities.size(), 0);
             _entities.push_back({newId, ComponentMask()});
@@ -70,7 +78,7 @@ public:
         EntityIndex index = _freeEntities.back();
         _freeEntities.pop_back();
         EntityId newId(index, _entities[index]._id.GetVersion());
-        _entities[index] = newId;
+        _entities[index] = {newId, ComponentMask()};
         return newId;
     }
 
@@ -79,22 +87,90 @@ public:
         int componentId = T::Id();
         if (componentId >= _componentPools.size()) {
             _componentPools.resize(componentId + 1);
+            if (_componentPools.size() >=kMaxComponents) {
+                assert(false);
+            }
         }
         ComponentPool& pool = _componentPools[componentId];
         if (!pool.IsInit()) {
             pool.Init(sizeof(T));
         }
         T* pComp = new (pool.Get(id.GetIndex())) T();
-        _entities[id.GetIndex()].set(componentId);
+        _entities[id.GetIndex()]._mask.set(componentId);
         return pComp;
     }
 
+    template <typename T>
+    T* GetComponent(EntityId id) {
+        int componentId = T::Id();
+        EntityInfo const& entity = _entities[id.GetIndex()];
+        if (!entity._mask.test(componentId)) {
+            return nullptr;
+        }
+        T* pComp = static_cast<T*>(_componentPools[componentId].Get(id.GetIndex()));
+        return pComp;
+    }
+
+    // Index by and EntityId
     std::vector<EntityInfo> _entities;
+
     std::vector<EntityIndex> _freeEntities;
+
+    // Indexed by ComponentId
+    // LMAO THIS IS ACTUALLY TERRIBLE. We can't have this be a resizable vector.
     std::vector<ComponentPool> _componentPools;
 };
 
+extern int s_componentCounter;
+template <class T>
+int GetId()
+{
+  static int s_componentId = s_componentCounter++;
+  return s_componentId;
+}
+
+struct ComponentFoo {
+    static int Id() { return 0; }
+    std::string _theStr;
+    float _theFloat = 2.f;
+};
+
+struct ComponentBar {
+    static int Id() { return 1; }
+    bool _theBool = true;
+    int _theInt = -3;
+};
+
 int main() {
+    Scene scene;
+    EntityId entityId1 = scene.AddEntity();
+    {
+        ComponentFoo* foo = scene.AddComponent<ComponentFoo>(entityId1);
+        assert(foo);
+        foo->_theStr = "howdy";
+
+        ComponentBar* bar = scene.AddComponent<ComponentBar>(entityId1);
+        assert(bar);
+        bar->_theInt = 999;
+
+        int blah = 0;
+    }
+
+    EntityId entityId2 = scene.AddEntity();
+    {
+        ComponentBar* bar = scene.AddComponent<ComponentBar>(entityId2);
+        assert(bar);
+        bar->_theInt = 4;
+    }
+
+    ComponentFoo* foo = scene.GetComponent<ComponentFoo>(entityId1);
+    assert(foo->_theStr == "howdy");
+
+    ComponentBar* bar = scene.GetComponent<ComponentBar>(entityId1);
+    assert(bar->_theInt == 999);
+
+    bar = scene.GetComponent<ComponentBar>(entityId2);
+    assert(bar->_theInt == 4);
 
     return 0;
 }
