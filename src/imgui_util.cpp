f948466d53e484ad3a1c6f22d67b269ff3b79590
Luis Torres <lgtorres42@gmail.com>
2023-10-09 11:03:24 -0500

    - Show entity name in editor ID imgui - bill evans progress

--- src/imgui_util.cpp
+++ src/imgui_util.cpp
@@ -8,10 +8,18 @@ extern GameManager gGameManager;
 
 namespace imgui_util {
 
-bool InputEditorId(char const* label, EditorId* v) {
+bool InputEditorId(char const* label, EditorId* v) {	
+	ImGui::PushItemWidth(50);
 	bool result = ImGui::InputScalar(label, ImGuiDataType_S64, &(v->_id), 0, 0, 0, ImGuiInputTextFlags_EnterReturnsTrue);
+	ImGui::PopItemWidth();
 	ImGui::SameLine();
-	ne::Entity* e = gGameManager._editor->ImGuiEntitySelector("Browse", "EntityPopup");
+	ne::Entity* e = gGameManager._neEntityManager->FindEntityByEditorId(*v);
+	if (e) {
+		ImGui::Text("%s (%s)", e->_name.c_str(), ne::gkEntityTypeNames[(int)e->_id._type]);
+	} else {
+		ImGui::Text("<Entity not found>");
+	}
+	e = gGameManager._editor->ImGuiEntitySelector("Browse", "EntityPopup");
 	if (e != nullptr) {
 		*v = e->_editorId;
 	}
